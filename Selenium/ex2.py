2f9483bad912e42847ef05b2e3b5e1eb1feded60
Ümit Öztürk <umit.ozturk.9716@gmail.com>
2017-06-06 02:21:31 +0300

    Google Form Filing Example with Selenium Module

--- Selenium/ex2.py
+++ Selenium/ex2.py
@@ -1,18 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+## ▬▶ Adding selenium module  
+## ▬▶ Adding time module for sleep function
 
 
-brow = webdriver.Firefox()
-brow.get("https://goo.gl/G47fJj")
-brow.find_element_by_name("entry.132484528").send_keys("Ümit Öztürk")
+brow = webdriver.Firefox()  ### ▬▶ Geckodriver sayesinde firefox browserı açar
+brow.get("https://goo.gl/G47fJj") ### ▬▶ Parametredeki siteye gider ▬▶ Bu örnekte google form
+brow.find_element_by_name("entry.132484528").send_keys("Ümit Öztürk") ### ▬▶ Elementin nameine göre elementi yakalar ve send_keys ile istediğimiz parametreyi vermemizi sağlar
+#sleep(1) 
+brow.find_element_by_name("entry.1327343241").send_keys("umit@pailab.net") ### ▬▶ Elementin nameine göre elementi yakalar ve send_keys ile istediğimiz parametreyi vermemizi sağlar
 #sleep(1)
-brow.find_element_by_name("entry.1327343241").send_keys("umit@pailab.net")
+brow.find_element_by_xpath(".//*[@id='mG61Hd']/div/div[2]/div[3]/div[1]/div/div/content/span").click() ### ▬▶ XPATH'e göre elementi yakalar ve send_keys ile istediğimiz parametreyi vermemizi sağlar
+sleep(5) ### ▬▶ 5 program saniye bekler
+brow.find_element_by_xpath(".//*[@id='mG61Hd']/div/div[2]/div[2]/div[2]/div[2]/div/content/div/label[3]/div/div[1]/div[3]/div").click() ### ▬▶ XPATH'e göre elementi yakalar ve send_keys ile istediğimiz parametreyi vermemizi sağlar
 #sleep(1)
-brow.find_element_by_xpath(".//*[@id='mG61Hd']/div/div[2]/div[3]/div[1]/div/div/content/span").click()
-sleep(5)
-brow.find_element_by_xpath(".//*[@id='mG61Hd']/div/div[2]/div[2]/div[2]/div[2]/div/content/div/label[3]/div/div[1]/div[3]/div").click()
+brow.find_element_by_xpath(".//*[@id='mG61Hd']/div/div[2]/div[2]/div[3]/div[2]/div/content/div/label[4]/div/div[1]/div[3]/div").click() ### ▬▶ XPATH'e göre elementi yakalar ve send_keys ile istediğimiz parametreyi vermemizi sağlar
 #sleep(1)
-brow.find_element_by_xpath(".//*[@id='mG61Hd']/div/div[2]/div[2]/div[3]/div[2]/div/content/div/label[4]/div/div[1]/div[3]/div").click()
-#sleep(1)
-brow.find_element_by_xpath(".//*[@id='mG61Hd']/div/div[2]/div[3]/div[1]/div/div[2]/content").click()
+brow.find_element_by_xpath(".//*[@id='mG61Hd']/div/div[2]/div[3]/div[1]/div/div[2]/content").click() ### ▬▶ XPATH'e göre elementi yakalar ve send_keys ile istediğimiz parametreyi vermemizi sağlar
