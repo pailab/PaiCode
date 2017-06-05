dcfe11c4a59191534b0ad98ea6902c588cc3538a
Ümit Öztürk <umit.ozturk.9716@gmail.com>
2017-06-06 02:10:17 +0300

    Google Search Example with Selenium Module

--- Selenium/ex1.py
+++ Selenium/ex1.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys 
+## ▬▶ Adding selenium module  
 
-brow = webdriver.Firefox()
-brow.get('https://www.google.com.tr')
-search = brow.find_element_by_name("q")
-search.send_keys("Google - Çeviri")
-search.send_keys(Keys.RETURN)
+
+brow = webdriver.Firefox()  ### ▬▶ Geckodriver sayesinde firefox browserı açar
+brow.get('https://www.google.com.tr') ### ▬▶ Parametredeki siteye gider
+search = brow.find_element_by_name("q") ### ▬▶ name 'i  "q" olan elementi bulur ▬▶ Bu örnekte google searchbar
+search.send_keys("Google - Çeviri") ### ▬▶ Seçilen elemente parametredeki veriyi girer.
+search.send_keys(Keys.RETURN) ### ▬▶ Sonuçların bize dönmesini sağlar
